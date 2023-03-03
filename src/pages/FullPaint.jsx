f40d324787819a5f65a744ac38fb3bc8ad1692d2
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-03-03 23:36:26 +0300

    single item page done

--- src/pages/FullPaint.jsx
+++ src/pages/FullPaint.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, selectCartItemById } from '../redux/slices/cartSlice';
 
 export const FullPaint = () => {
   const [paints, setPaints] = React.useState();
   const { id } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cartItem = useSelector(selectCartItemById(id));
+  const [activeType, setActiveType] = React.useState(0);
+  const [activeSize, setActiveSize] = React.useState(0);
+  const typeNames = ['акриловая', 'эмалевая', 'лаковая'];
+
+  const addedCount = cartItem ? cartItem.count : 0;
 
   React.useEffect(() => {
     async function fetchPaints() {
@@ -21,13 +30,67 @@ export const FullPaint = () => {
     fetchPaints();
   }, []);
 
+  const onClickAdd = () => {
+    const item = {
+      id: paints.id,
+      title: paints.title,
+      price: paints.price,
+      imageUrl: paints.imageUrl,
+      type: typeNames[activeType],
+      size: paints.sizes[activeSize],
+    };
+
+    dispatch(addItem(item));
+  };
+
   if (!paints) return 'Загрузка...';
 
   return (
-    <div>
-      <img src={paints.imageUrl} alt='Photo' />
-      <h2>{paints.title}</h2>
-      <h4>{paints.price}</h4>
+    <div className='single-model'>
+      <img src={paints.imageUrl} alt='Photo' className='single-model__img' />
+      <div className='single-model__info'>
+        <h2 className='single-model__title'>{paints.title}</h2>
+        <div className='model-block__selector'>
+          <ul className='single-model__types'>
+            {paints.types.map((typeId) => (
+              <li
+                key={typeId}
+                onClick={() => setActiveType(typeId)}
+                className={activeType === typeId ? 'active' : ''}>
+                {typeNames[typeId]}
+              </li>
+            ))}
+          </ul>
+          <ul className='single-model__sizes'>
+            {paints.sizes.map((size, i) => (
+              <li
+                key={size}
+                onClick={() => setActiveSize(i)}
+                className={activeSize === i ? 'active' : ''}>
+                {size} мл.
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='model-block__bottom'>
+          <div className='model-block__price'>от {paints.price} ₽</div>
+          <button onClick={onClickAdd} className='button button--outline button--add'>
+            <svg
+              width='12'
+              height='12'
+              viewBox='0 0 12 12'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
+                fill='white'
+              />
+            </svg>
+            <span>Добавить</span>
+            {addedCount > 0 && <i>{addedCount}</i>}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
